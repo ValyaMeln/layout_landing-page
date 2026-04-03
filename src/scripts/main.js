b129334fa152a190cac5575ffb826526193d14d6
Valentyna <valya0322@ukr.net>
2026-04-03 10:08:25 +0300

    refactoring the margin-bottom of the logo and quotes in the JS file

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,8 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('.contact-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    e.target.reset();
+  });
+});
